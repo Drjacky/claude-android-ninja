39963f164f9642ab2b576a8558f92a6c0091456c
Drjacky <dr.jacky.2005@gmail.com>
2026-03-25 11:35:47 +0000

    Update Room plugin to version 3 and include bundled SQLite driver

--- assets/convention/AndroidRoomConventionPlugin.kt
+++ assets/convention/AndroidRoomConventionPlugin.kt
@@ -1,9 +1,9 @@
 /*
  * Convention plugin for Room database modules
- * Configures: Room plugin, KSP, schema directory
+ * Configures: Room 3 plugin, KSP, schema directory, bundled SQLite driver
  */
 
-import androidx.room.gradle.RoomExtension
+import androidx.room3.gradle.RoomExtension
 import com.google.devtools.ksp.gradle.KspExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
@@ -14,23 +14,23 @@ import org.gradle.kotlin.dsl.dependencies
 class AndroidRoomConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
-            apply(plugin = "androidx.room")
+            apply(plugin = "androidx.room3")
             apply(plugin = "com.google.devtools.ksp")
 
             extensions.configure<KspExtension> {
                 arg("room.generateKotlin", "true")
             }
 
-            extensions.configure<RoomExtension> {
+            extensions.configure<RoomExtension>("room3") {
                 // Schema directory for Room auto migrations
-                // See https://developer.android.com/reference/kotlin/androidx/room/AutoMigration
+                // See https://developer.android.com/reference/kotlin/androidx/room3/AutoMigration
                 schemaDirectory("$projectDir/schemas")
             }
 
             dependencies {
-                add("implementation", libs.findLibrary("room.runtime").get())
-                add("implementation", libs.findLibrary("room.ktx").get())
-                add("ksp", libs.findLibrary("room.compiler").get())
+                add("implementation", libs.findLibrary("room3.runtime").get())
+                add("implementation", libs.findLibrary("androidx.sqlite.bundled").get())
+                add("ksp", libs.findLibrary("room3.compiler").get())
             }
         }
     }
